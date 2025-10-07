1ac5026e45b8a65c853f13d2c7d90a3466159105
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2025-10-07 08:26:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 // ------------- FILE HEADER -------------
 // Author ✅: Rylee Blaubergs
 // Assignment ✅: 2
-// Date ✅: 10/6/2025
-// Citations: zyBooks
+// Date ✅: 10/7/2025
+// Citations: zyBooks, Assignment example (for how to use setprecision)
 
 
 // ------------- ZYBOOKS SCORES -------------
@@ -33,17 +33,37 @@
 
 // ------------- CODE -------------
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
-
-// Function prototypes (if any)
-
 
 // Main function
 // https://en.cppreference.com/w/cpp/language/main_function.html
 int main(int argc, char* argv[]) {
-  cout << "Hello, World!" << endl;
-  return 0;
+
+   const int SERVING = 5; 
+   const int CALORIESPERSERVING = 160;
+   double numServings;
+   double calories;
+   int numOreos;
+   
+   cout << "Welcome to the Oreo Calculator!" << endl;
+   cout << endl;
+   cout << "Enter the amount of Oreos eaten: ";
+   cin >> numOreos; 
+   cout << endl;
+   
+   numServings = static_cast<double>(numOreos) / SERVING;
+   calories = numServings * CALORIESPERSERVING;
+
+  // sets decimal displayed to the first value (a tenth) 0.0, 1.5, 2.2, etc...
+   cout << fixed << setprecision(1);
+   
+   cout << numOreos << " Oreos equals " << numServings << " servings!" << endl;
+   cout << "You consumed " << calories << " calories." << endl;
+   cout << endl;
+   cout << "Keep eating Oreos!";
+   
+   return 0;
 }
 
 // Function implementations (if any)
@@ -51,30 +71,58 @@ int main(int argc, char* argv[]) {
 
 // ------------- DESIGN -------------
 /* 
-Program Name:
+Program Name: Oreo Calculator
 
-Program Description:
+Program Description: Calculates amount of oreos inputted into the amount of servings and calories of oreos inputted
 
 Design:
 A. INPUT
-Define the input variables including name data type. 
+number of oreos as an INT
 
 B. OUTPUT
-Define the output variables including data types. 
+number of servings as an Double
+number of caloriesPerServing as an Double
 
 C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
+take number of oreos and divide by servings
+take number of servings and multiply by caloriesPerServing
 
 D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+DECLARE oreos as an INT
+DECLARE numServings as a double
+DECLARE calories as a double
+DECLARE servings as a constant INT
+DECLARE calories as a constant INT
 
+DISPLAY welcome message
+PROMPT amount of oreos to be calculated
+INPUT number of oreos
+SET numServings = static_cast<double>(numOreos) / SERVING;
+SET calories = numServings * CALORIESPERSERVING;
 
-SAMPLE RUNS
-Copy from assignment document.
+DISPLAY oreos " Oreos equals " numServings " servings!"
+DISPLAY "You consumed " calories " calories."
+
+DISPLAY goodbye message
+
+SAMPLE RUNS:
+
+Welcome to the Oreo Calculator!
+
+Enter the amount of Oreos eaten: 12
+
+12 Oreos equals 2.4 servings!
+You consumed 384.0 calories.
+
+Keep eating Oreos! 
+********************************************************
+Welcome to the Oreo Calculator!
+
+Enter the amount of Oreos eaten: 3
+
+3 Oreos equals 0.6 servings!
+You consumed 96.0 calories.
+
+Keep eating Oreos!
 
 */
