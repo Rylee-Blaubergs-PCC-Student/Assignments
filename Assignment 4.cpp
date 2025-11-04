c583a0996dff06ad21e4bd75146402edcfcbb4e1
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2025-11-04 14:52:53 -0800

    Update Assignment 4.cpp

--- Assignment 4.cpp
+++ Assignment 4.cpp
@@ -41,12 +41,44 @@ using namespace std;
 // https://en.cppreference.com/w/cpp/language/main_function.html
 int main(int argc, char* argv[]) {
 
-string finalText = " ";
 string phrase1 = " ";
 string phrase2 = " ";
+string finalText = " ";
 string tmpStr = " ";
 
+int index1 = 0;
+int index2 = 0;
+
+
 cout << "Welcome to my Phrases and Subphrases Program!" << endl;
+
+cout << "Enter Phrase 1: ";
+getline(cin, phrase1);
+cout << "You Entered: " << phrase1 << endl;
+
+cout << "Enter Phrase 2: ";
+getline(cin, phrase2);
+cout << "You Entered: " << phrase2 << endl;
+cout << endl;
+
+index1 = phrase1.find(phrase2);
+index2 = phrase2.find(phrase1);
+
+if (phrase1 == phrase2) {
+    cout << "Both phrases match" << endl;
+    }
+
+if (index1 != string::npos) {
+    cout << "Found phrase 2 in phrase 1." << endl;
+    cout << phrase1.substr(index1) << endl;
+    
+} else if (index2 != string::npos) {
+    cout << "Found phrase 1 in phrase 2." << endl;
+    cout << phrase2.substr(index2) << endl;  
+    
+} else {
+   cout << "No match" << endl;    
+}
 
    return 0;
 }
