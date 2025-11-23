942a31288e2556851fc77a27e547365b815e2e45
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2025-11-22 22:57:55 -0800

    Update Assignment 5.cpp

--- Assignment 5.cpp
+++ Assignment 5.cpp
@@ -1,15 +1,15 @@
 // ------------- FILE HEADER -------------
 // Author ✅: Rylee Blaubergs
 // Assignment ✅: 5
-// Date ✅: 
+// Date ✅: 11/22/2025
 // Citations: zyBooks
 
 
 // ------------- ZYBOOKS SCORES -------------
-// Chapter ✅: 
+// Chapter ✅: 5
 // Participation ✅:
 // Challenge ✅: 
-// Labs ✅: 
+// Labs ✅: 100%
 
 
 // ------------- DISCORD POSTS -------------
@@ -159,16 +159,95 @@ Program Description:
 
 Design:
 A. INPUT
-
+number of people as an int
+average number of slices as a double
+cost of pizza as a double
 
 B. OUTPUT
+Inside do while loop:
+number of pizzas to order
+cost of pizza on current entry
+tax amount
+delivery fee
+total aomunt for current entry
 
+After do while loop finishes
+Total number of entries
+total number of pizzas to be ordered
+average number of pizza
+max number of people
+max cost of pizza
 
 C. CALCULATIONS
+// calculate the number of slices total
+slices = numPeople * averageNumSlices;
 
+// turn total slices into number of pizzas
+fractionalPizza = slices / SLICES_PER_PIZZA;
+
+// round amount of pizzas up
+numPizzas = ceil(fractionalPizza);
+
+// calculate the rest
+costOfPizzas = numPizzas * pizzaCost;
+tax = costOfPizzas * TAX;
+deliveryCost = (costOfPizzas + tax) * DELIVERY_FEE;
+
+// calculate total amount for current entry
+totalCost = costOfPizzas + tax + deliveryCost;
 
 D. LOGIC and ALGORITHMS
 
+DECLARE INT'S:
+numPeople
+numPizzas
+entries
+totalNumPizza
+maxNumPeople
+
+DECLARE DOUBLE'S
+averageNumSlices
+pizzaCost 
+costOfPizzas 
+tax 
+deliveryCost 
+totalCost 
+slices 
+fractionalPizza
+maxCostPizza 
+averageNumPizza 
+
+DECLARE CONSTANT'S
+SLICES_PER_PIZZA as a const int
+TAX as a const double
+DELIVERY_FEE as a const double
+
+DECLARE done as string
+
+PRINT Welcome message
+SET floating number or doubles to be printed at the second decimal place 0.00
+START DO WHILE:
+ PROMPT user for the numPeople, averageNumSlices, and pizzaCost
+ CALCULATE number of pizzas for current entry
+ CALCULATE costOfPizzas
+ CALCULATE tax
+ CALCULATE deliveryCost
+ CALCULATE totalCost for current entry
+ DISPLAY all calculations
+ CHECK for new max number of people and max cost of pizza
+ PROMPT user if they want to keep adding entries
+ WHILE (done != "n")
+AFTER do while
+
+CALCULATE average number of pizza
+
+PRINT totals:
+entries
+totalNumPizza
+averageNumPizza
+maxNumPeople
+maxCostPizza
+DISPLAY goodbye message
 
 SAMPLE RUNS:
 Welcome to my Pizza Party Statistics program!
